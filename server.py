1056416d19a71b83e0a2a0e308f9dc6d699b80b8
Jun <junsoopark92@gmail.com>
2022-09-15 15:39:10 -0400

    make handle function

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from email import message
 from http import client
 import socket
 import threading
@@ -10,13 +11,48 @@ server.bind((HOST, PORT))
 
 server.listen()
 
-client = []
+clients = []
 nicknames = []
 
 #broadcast
 def broadcast(message):
     for client in clients:
         client.send(message)
-#receive
+
 
 #handle
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            print(f"{nicknames[clients.index(client)]}")
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            break
+
+
+#receive
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}!")
+
+        client.send("NICK".encode('utf-8'))
+        nickname = client.recv(1024)
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname of the client is {nickname}")
+        broadcast(f"{nickname" connected to the server! \n".encode('utf-8'))
+        client.send("Connected to the server".encode('utf-8')) 
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+
